9920b8d3dcb0a499fcc48075229b7b308cf568a5
Talha Ahsan <stwyde@gmail.com>
2018-02-17 09:31:51 -0600

    Scrubbed API out
    Please no more hijacking

--- lib/BonoboDraftFile.rb
+++ lib/BonoboDraftFile.rb
@@ -2,10 +2,9 @@
 
 require 'discordrb'
 
-#   bonobo_discord_api_key=NDEzMjA2MjUwOTUyNzIwMzk0.DWi0qg.ihlWz76mDsWL9kjOrHvPTFODokk ruby my_bot.rb
 
 
-bot = Discordrb::Bot.new token: ENV['bonobo_discord_api_key'], client_id: 413206250952720394
+bot = Discordrb::Bot.new token: ENV['bonobo_discord_api_key'], client_id: ENV['discord_client_id']
 
 puts "This bot's invite URL is #{bot.invite_url}."
 puts 'Click on it to invite it to your server.'
